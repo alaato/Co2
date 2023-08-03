5e4aee3946d52c2c3b7dce8f83be7f6eab2de77e
Alaa <toalaa9@gmail.com>
2023-08-03 18:11:42 +0200

    final

--- Server/app.js
+++ Server/app.js
@@ -5,11 +5,28 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 app.use(cors());
 const io = new Server(server, {
   cors: {
     origin: '*'
   }
+});
+main().catch(err => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGODB);
+  } catch (error) {
+    handleError(error);
+  }}
+
+// creating a model for the readings collection
+const Data = mongoose.model('reading', {reading: Number, quality: String, date: String});
+
+Data.watch().on("change", data => {
+        io.emit('data', data.fullDocument);
 });
 
 // Function to generate random CO2 values between 400 and 3500 ppm
@@ -31,26 +48,23 @@ const makeData = function()
 {
   const reading = randomPpm();
   const quality = checkQuality(reading);
-  const date = new Date(Date.now()).toUTCString().slice(4, -4).replace('2023', '');
+  const date = new Date(Date.now()).toString().slice(4, -40).replace('2023', '');
   const data = {reading: reading, quality: quality, date: date}
   return data;
 }
 
 
-app.get('/api/data', (req, res) => {
-  const data = [];
-  for(let i = 0; i <10; i++)
-  {
-    data.push(makeData());
-  }
-  console
+app.get('/api/data', async(req, res) => {
+  const data = await Data.find({}).sort({_id:-1}).limit(10).exec();
   res.json(data);
 })
 
 // Send CO2 values to all connected clients every 10 seconds
 setInterval(async() => {
-  const data = makeData();
-  io.emit('data', data);
+  const data = new Data(makeData())
+  console.log(data)
+
+  await data.save();
 }, 10000);
 
 // Start the server
